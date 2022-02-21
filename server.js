e83488d64025c91755b93bc6788cbed5ee2c2ebc
TanujaSawai <tanusawai8275@gmail.com>
2022-02-21 18:28:08 +0530

    Added by tanuja

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ var onAboutUs=function(req, res){
     res.send("Chief Mentor :Ravi Tambade");
 };
 var onDefault=function(req, res){
-    res.send("<h1>Transflower Learning Pvt. Ltd</h1>"+
+    res.send("<h1>Fabrikam multinationals</h1>"+
               "<hr/>"+
-              "<ol>"+
-                    " <li>Mentoring for skill building</li>"+
-                    " <li>Workshops</li>"+
-                    " <li>Online Course</li>"+
-                    " <li>Corporate Training</li>"+
-             "</ol>");
+              <p>Fabrikam is a multinational company has their 24 Offices all over world. Company wanted to build and host Information Management Solutions in web. Company also wants to host internet public web site for their potential customers to know about products and services provided. They required to build and host intranet application for all employees to access Human Resource, Accounting, Project Management related information. Fabrikam has public Cloud subscription to use it as Infrastructure as a Service ( I-a-a-S) and Platform As A Service.
+
+              </p>
+
 };
 app.get("/",onDefault);  // Request handler functions are registered
 app.get("/aboutus",onAboutUs);  // Request handler functions are registered
